550b88b98bb33f64dbc9c1da913d488ac9127b36
yavuz.bekar <yavuz.bekar@albertmedya.com>
2023-12-07 20:04:10 +0300

    last fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Quiz from './Components/Quiz/Quiz';
 
 function App() {
+  const [activeQuestion, setActiveQuestion] = useState(0)
   const [questions, setQuestions] = useState([
     {
       id: 1,
@@ -12,11 +13,92 @@ function App() {
       c: "Javascript",
       d: "MSQL",
       correct: "d"
-    }
+    },
+    {
+      id: 1,
+      question: "Hangisi javascript geliştirme kütüphanesi değildir?",
+      a: "Vue",
+      b: "PHP",
+      c: "React",
+      d: "Anguler",
+      correct: "b"
+    },
+    {
+      id: 3,
+      question: "Hangi etiket bir başlık belirtir?",
+      a: "<p>",
+      b: "<div>",
+      c: "<h1>",
+      d: "<span>",
+      correct: "c"
+    },
+    {
+      id: 4,
+      question: "JavaScript'te bir değişkeni tanımlamak için kullanılan keyword nedir?",
+      a: "var",
+      b: "let",
+      c: "const",
+      d: "variable",
+      correct: "c"
+    },
+    {
+      id: 5,
+      question: "CSS'te bir öğenin kenar yarıçapını ayarlamak için kullanılan özellik hangisidir?",
+      a: "border-style",
+      b: "border-radius",
+      c: "margin",
+      d: "padding",
+      correct: "b"
+    },
+    {
+      id: 6,
+      question: "Hangi HTTP metodu veri göndermek için kullanılır?",
+      a: "GET",
+      b: "POST",
+      c: "DELETE",
+      d: "PUT",
+      correct: "b"
+    },
+    {
+      id: 7,
+      question: "JavaScript'te bir döngüyü sonlandırmak için kullanılan ifade nedir?",
+      a: "break",
+      b: "stop",
+      c: "exit",
+      d: "return",
+      correct: "a"
+    },
+    {
+      id: 8,
+      question: "Hangi dil bir tarayıcı tarafından doğrudan yorumlanan bir dildir?",
+      a: "Java",
+      b: "C#",
+      c: "Python",
+      d: "JavaScript",
+      correct: "d"
+    },
+    {
+      id: 9,
+      question: "Hangi HTML etiketi bir resim eklemek için kullanılır?",
+      a: "<img>",
+      b: "<picture>",
+      c: "<figure>",
+      d: "<image>",
+      correct: "a"
+    },
+    {
+      id: 10,
+      question: "React'ta bir bileşenin state'ini güncellemek için kullanılan fonksiyon hangisidir?",
+      a: "updateState",
+      b: "changeState",
+      c: "setState",
+      d: "modifyState",
+      correct: "c"
+    },
   ])
   return (
     <div className='app'>
-      <Quiz questions={questions}></Quiz>
+      <Quiz activeQuestion={activeQuestion} setActiveQuestion={setActiveQuestion} questions={questions}></Quiz>
     </div>
   );
 }
--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,47 +1,85 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Quiz.css";
 
-function Quiz({ questions }) {
-    const [answer, setAnswer] = useState()
+function Quiz({ questions, activeQuestion, setActiveQuestion }) {
+    const [answer, setAnswer] = useState();
+    const [handleAnswer, setHandleAnswer] = useState();
+    const [click, setClick] = useState(false);
+    const [score, setScore] = useState(0)
 
-    const handleAnswer = (e) => {
-        setAnswer(e)
-    }
-    console.log(answer)
+    useEffect(() => {
+        setAnswer(undefined);
+        if (handleAnswer) {
+            setScore(score + 1)
+        }
+        setHandleAnswer(undefined)
+    }, [activeQuestion]);
+
+    const checkAnswer = (e) => {
+        setClick(false);
+        setAnswer(e);
+    };
+
+    const handleClick = () => {
+        setClick(true);
+        if (answer === questions[activeQuestion].correct) {
+            setHandleAnswer(true);
+        } else {
+            setHandleAnswer(false);
+        }
+
+    };
+
     return (
         <div>
-            {
-                questions.map((question) => (
-                    <div className='question-card' key={question.id}>
-                        <div className='question'>
-                            {question.question}
-                        </div>
-                        <div className='answers-card'>
-                            <div className='answers'>
-                                <input onClick={() => handleAnswer("a")} type='radio' name={`question_${question.id}`} />
-                                <div className='answer'>{question.a}</div>
-                            </div>
-                            <div className='answers'>
-                                <input onClick={() => handleAnswer("b")} type='radio' name={`question_${question.id}`} />
-                                <div className='answer'>{question.b}</div>
-                            </div>
-                            <div className='answers'>
-                                <input onClick={() => handleAnswer("c")} type='radio' name={`question_${question.id}`} />
-                                <div className='answer'>{question.c}</div>
-                            </div>
-                            <div className='answers'>
-                                <input onClick={() => handleAnswer("d")} type='radio' name={`question_${question.id}`} />
-                                <div className='answer'>{question.d}</div>
-                            </div>
-                        </div>
-                        <div>
-
-                        </div>
+            <div className='score'>{`Skor : ${score}`}</div>
+            <div className='transition'>
+                <button onClick={() => setActiveQuestion(activeQuestion - 1)} disabled={activeQuestion === 0} className='previous'>Önceki Soru</button>
+                <button onClick={() => setActiveQuestion(activeQuestion + 1)} disabled={questions.length === (activeQuestion + 1)} className='next'>Sonraki Soru</button>
+            </div>
+            <div className='question-card' >
+                <div className='question'>
+                    {questions[activeQuestion].question}
+                </div>
+                <div className='answers-card'>
+                    <div className='answers'>
+                        <input onClick={() => checkAnswer("a")} type='radio' name={`question_${questions[activeQuestion].id}`} checked={answer === "a"} />
+                        <div className='answer'>{questions[activeQuestion].a}</div>
                     </div>
-                ))
-            }
+                    <div className='answers'>
+                        <input onClick={() => checkAnswer("b")} type='radio' name={`question_${questions[activeQuestion].id}`} checked={answer === "b"} />
+                        <div className='answer'>{questions[activeQuestion].b}</div>
+                    </div>
+                    <div className='answers'>
+                        <input onClick={() => checkAnswer("c")} type='radio' name={`question_${questions[activeQuestion].id}`} checked={answer === "c"} />
+                        <div className='answer'>{questions[activeQuestion].c}</div>
+                    </div>
+                    <div className='answers'>
+                        <input onClick={() => checkAnswer("d")} type='radio' name={`question_${questions[activeQuestion].id}`} checked={answer === "d"} />
+                        <div className='answer'>{questions[activeQuestion].d}</div>
+                    </div>
+                </div>
+                <div className='answer-button'>
+                    <button onClick={handleClick}>Cevabı Gönder</button>
+                </div>
+                {
+                    answer !== undefined &&
+                    (
+                        click &&
+                        <div className='answer-area'>
+                            {
+                                handleAnswer ? (
+                                    <div>Cevap Doğru</div>
+                                ) : (
+                                    <div>Cevap Yanlış </div>
+                                )
+                            }
+                        </div>
+                    )
+                }
+            </div>
         </div>
-    )
+    );
 }
 
 export default Quiz;
